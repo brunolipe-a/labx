45df42481ddd9a16d0a9c7e28ba834dd8521278e
Bruno Felipe <bfaa1999@gmail.com>
2020-09-23 23:29:03 -0300

    test: passando responsabilidade dos testes para cada pacote

--- packages/web/jest.config.js
+++ packages/web/jest.config.js
@@ -7,5 +7,20 @@ module.exports = {
   name,
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/packages/web/src/' }),
   preset: 'ts-jest',
-  setupFiles: ["./src/setupTests.ts"],
+  clearMocks: true,
+  testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "!./src/setupTests.ts",
+    "!**/coverage/**",
+    "!**/node_modules/**",
+    "!**/jest.setup.js"
+  ],
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  coverageReporters: [
+    "lcov"
+  ],
+  setupFilesAfterEnv: ["./src/setupTests.ts"],
 };
